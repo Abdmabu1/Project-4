8419c83be955ce97214221c977a36ef4feac6daf
Chesirekiptui <kbmuraya@gmail.com>
2021-08-01 02:54:16 +0300

    Added branch 7

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-"""App entry point."""
-from flask_sqlalchemy_tutorial import create_app
+"""Application entry point."""
+from flask_login_tutorial import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0')
